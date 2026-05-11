9d14e1f12e6b8dc9f05d456761880e7cf1c9b6bc
tahmidhossaindev <tahmidhossain345@gmail.com>
2026-05-11 15:17:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let username = "se9k"
+let email = "tahmidhossain345@gmail.com"
+let number = 1816400681
+let password = "tahmid134"
+
+if (username == "se9k") {
+    if (email == "tahmidhossain345@gmail.com") {
+        if (number == 1816400681) {
+
+            if (password == "tahmid1234") {
+
+                console.log("Log In Succesful!");
+
+            }
+
+            else {
+                console.log("Password Did Not Match!");
+
+            }
+        }
+
+        else {
+            console.log("Number Did Not Match!");
+
+        }
+    }
+
+    else {
+        console.log("Email Did Not Match!");
+
+    }
+
+}
+
+else {
+    console.log("Username Not Found!");
+
+}
